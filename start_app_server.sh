08c98925ce12fd4857c0f7a203b8c4d0abe1b25d
Wee Liat <wl@hackertrail.com>
2018-10-03 16:32:44 +0800

    added hardcoded mongo credentials

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
+export MONGODB_CREDENTIALS=mongodb://weeliat:helloworld1@ds121753.mlab.com:21753/merry-pear
 
 # finally, run the app in dev mode...
 npm run dev
