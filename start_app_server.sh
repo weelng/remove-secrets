5c9fd65fe982486eee779fd7e2db84a896016957
Wee Liat <wl@hackertrail.com>
2018-10-03 16:46:02 +0800

    removed secret!

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://weeliat:helloworld1@ds121753.mlab.com:21753/merry-pear
+# export MONGODB_CREDENTIALS=mongodb://weeliat:helloworld1@ds121753.mlab.com:21753/merry-pear
 
 # finally, run the app in dev mode...
 npm run dev
